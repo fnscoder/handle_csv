f86d6536926fd1f1d5f4a6df78306f8938ecfb14
Felipe Souza <fnscoder@gmail.com>
2023-01-26 22:48:06 -0300

    Solved 2 using generator

--- generate_csv.py
+++ generate_csv.py
@@ -11,11 +11,24 @@ Add another argument for the filename to use for the CSV file.
 
 Usage: $ python generate_csv.py 5 test.csv
 It will generate a CSV file called test.csv with 5 rows
+
+If you had to generate a large number of rows (millions or more), is there
+anything you would do differently to handle this?
+Modify your script to handle this requirement.
+
+Changed to use a generator to generate the rows, this way we will not load all the rows in memory at the same time.
+The rows will be generated one by one.
 """
 import argparse
 import csv
 import random
 import string
+
+
+def generate_rows(num_rows):
+    for i in range(num_rows):
+        random_string = ''.join(random.choices(string.ascii_letters, k=random.randint(1, 100)))
+        yield (random.randint(1000, 9999), random_string)
 
 
 parser = argparse.ArgumentParser()
@@ -24,11 +37,7 @@ parser.add_argument('filename', type=str, help='Name of the CSV file to generate
 
 args = parser.parse_args()
 
-
-with open(args.filename, mode='w') as csv_file:
+with open(args.filename, mode='w', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(('id', 'random_string'))
-
-    for i in range(args.num_rows):
-        random_string = ''.join(random.choices(string.ascii_letters, k=random.randint(1, 100)))
-        writer.writerow((random.randint(1000, 9999), random_string))
+    writer.writerows(generate_rows(args.num_rows))
